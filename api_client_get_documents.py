4657b90306fbe0fbe122dc1eb6521a7b81fcaf94
V1aj3ro <viajero95@mail.ru>
2026-08-16 22:44:39 +0700

    Практикуемся в использовании HTTP API клиентов. Практика использования HTTP API-клиентов: получение документов по счету

---
+++ api_client_get_documents.py
@@ -0,0 +1,25 @@
+from clients.http.gateway.users.client import build_users_gateway_http_client
+from clients.http.gateway.documents.client import build_documents_gateway_http_client
+from clients.http.gateway .accounts.client import build_accounts_gateway_http_client
+
+users_gateway_client = build_users_gateway_http_client()
+documents_gateway_client = build_documents_gateway_http_client()
+accounts_gateway_client = build_accounts_gateway_http_client()
+
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+create_account_response = accounts_gateway_client.open_credit_card_account(
+    user_id = create_user_response['user']['id']
+)
+print('Create account response:', create_user_response)
+
+get_tariff_document_response = documents_gateway_client.get_tariff_document(
+    account_id = create_account_response['account']['id']
+)
+print('Get tariff document response:', create_user_response)
+
+get_contract_document_response = documents_gateway_client.get_contract_document(
+    account_id = create_account_response['account']['id']
+)
+print('Get contract document response:', create_user_response)
